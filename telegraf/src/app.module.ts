e4623f0695d664578d6493b6c5bb7707006ad297
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-08 23:30:07 +0600

    generate 4 module

--- telegraf/src/app.module.ts
+++ telegraf/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ReviewModule } from './review/review.module';
+import { ProductModule } from './product/product.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ReviewModule, ProductModule, TopPageModule],
   controllers: [AppController],
   providers: [AppService],
 })
