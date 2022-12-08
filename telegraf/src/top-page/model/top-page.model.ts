b7d29c87b330ec555399821b074039eca520072f
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-08 23:58:23 +0600

    init models for logic

---
+++ telegraf/src/top-page/model/top-page.model.ts
@@ -0,0 +1,26 @@
+export enum TopLevelCategory {
+    Cources,
+    Services,
+    Books,
+    Products
+}
+
+export class TopPageModel {
+    firstCategory: TopLevelCategory;
+    secondCategory: string;
+    title: string;
+    category: string // страница должна иметь категорию, по которой будут подтягиваться продукты
+    hh?: {
+        count: number; // всего вакансии
+        juniorSalary: number; 
+        middleSalary: number;
+        seniorSalary: number;
+    }
+    advantages: {
+        title: string;
+        description: string;
+    }[];
+    seoText: string;
+    tagsTitle: string; // переменный заголовок, будет меняться в зависимости от продукта
+    tags: string[]
+}
