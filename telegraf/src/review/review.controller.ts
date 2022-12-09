15183f2ebdc59819895e76d69633a6ce375c20b8
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-09 23:03:29 +0600

    feat: ReviewService added

--- telegraf/src/review/review.controller.ts
+++ telegraf/src/review/review.controller.ts
@@ -1,21 +1,30 @@
-import { ReviewModel } from './model/review.model';
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { REVIEW_NOT_FOUND } from './review.constant';
+import { ReviewService } from './review.service';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { Body, Controller, Delete, Get, HttpException, Param, Post, HttpStatus } from '@nestjs/common';
 
 @Controller('review')
 export class ReviewController {
 
-    @Post('create')
-    async create(@Body() dto: Omit<ReviewModel, '_id'>) { 
+    constructor(private readonly reviewService: ReviewService) {}
 
+    @Post('create')
+    async create(@Body() dto: CreateReviewDto) { 
+        this.reviewService.create(dto)
     }
 
     @Delete(':id')
     async delete(@Param('id') id: string) {
+        const deleteDoc = await this.reviewService.delete(id)
+        if(!deleteDoc) {
+            throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
+        }
+
 
     }
 
     @Get('byProduct/:productId')
     async getByProduct(@Param('productId') productId: string) {
-
+        return this.reviewService.findByProduct(productId)
     }
 }
--- telegraf/src/review/review.module.ts
+++ telegraf/src/review/review.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { ReviewController } from './review.controller';
 import { APP_CONSTANTS } from 'src/configs/constants';
+import { ReviewService } from './review.service';
 
 @Module({
   controllers: [ReviewController],
@@ -13,6 +14,7 @@ import { APP_CONSTANTS } from 'src/configs/constants';
         collection: APP_CONSTANTS.COLLECTIONS.REVIEW
       }
     }
-  ])]
+  ])],
+  providers: [ReviewService]
 })
 export class ReviewModule {}
