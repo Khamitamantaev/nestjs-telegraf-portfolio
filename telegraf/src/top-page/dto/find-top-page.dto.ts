d6e5e127991c1e1728af3e8f87dc74315ad1f689
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-12 23:07:14 +0600

    add top page controller service logic and guards

--- telegraf/src/top-page/dto/find-top-page.dto.ts
+++ telegraf/src/top-page/dto/find-top-page.dto.ts
@@ -1,4 +1,7 @@
+import { IsEnum } from 'class-validator';
 import { TopLevelCategory } from './../model/top-page.model';
 export class FindTopPageDto {
+    
+    @IsEnum(TopLevelCategory)
     firstCategory: TopLevelCategory
 }
