8f1d27db26414e2b5a044af956961cb3940e7666
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-12 19:06:11 +0600

    create simple validation pipe for mongo ID validation

--- telegraf/src/product/product.controller.ts
+++ telegraf/src/product/product.controller.ts
@@ -1,3 +1,4 @@
+import { IdValidationPipe } from './../pipes/id-validation.pipe';
 import { PRODUCT_NOT_FOUND } from './product.constants';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -17,7 +18,7 @@ export class ProductController {
     }
 
     @Get(':id')
-    async get(@Param('id') id: string) {
+    async get(@Param('id', IdValidationPipe) id: string) {
         const product = await this.productService.findById(id)
         if(!product) {
             throw new NotFoundException(PRODUCT_NOT_FOUND)
@@ -26,7 +27,7 @@ export class ProductController {
     }
 
     @Delete(':id')
-    async delete(@Param('id') id: string) {
+    async delete(@Param('id', IdValidationPipe) id: string) {
         const deletedProduct = await this.productService.deleteById(id)
         if(!deletedProduct) {
             throw new NotFoundException(PRODUCT_NOT_FOUND)
@@ -35,7 +36,7 @@ export class ProductController {
     }
 
     @Patch(':id')
-    async patch(@Param('id') id: string, @Body() dto: ProductModel) {
+    async patch(@Param('id', IdValidationPipe) id: string, @Body() dto: ProductModel) {
         const updatedProduct = await this.productService.updateById(id, dto)
         if(!updatedProduct) {
             throw new NotFoundException(PRODUCT_NOT_FOUND)
--- telegraf/src/review/review.controller.ts
+++ telegraf/src/review/review.controller.ts
@@ -16,6 +16,7 @@ import {
 } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { UserEmail } from 'src/decorators/user-email.decorator';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
 
 @Controller('review')
 export class ReviewController {
@@ -30,7 +31,7 @@ export class ReviewController {
 
     @UseGuards(JwtAuthGuard)
     @Delete(':id')
-    async delete(@Param('id') id: string) {
+    async delete(@Param('id', IdValidationPipe) id: string) {
         const deleteDoc = await this.reviewService.delete(id)
         if (!deleteDoc) {
             throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
@@ -39,7 +40,7 @@ export class ReviewController {
 
     @UseGuards(JwtAuthGuard)
     @Get('byProduct/:productId')
-    async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
+    async getByProduct(@Param('productId', IdValidationPipe) productId: string, @UserEmail() email: string) {
         console.log(email)
         return this.reviewService.findByProductId(productId)
     }
