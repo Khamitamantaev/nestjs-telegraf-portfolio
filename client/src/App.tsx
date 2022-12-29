a0c73527138d0a3a9cd1849f7fdf166d40d46eb5
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-30 02:12:58 +0600

    add react-app with simple header and footer

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './bootstrap.min.css'
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+
+const App = () => 
+<div className="App">
+  <Header/>
+  <Footer/>
+</div>
+
+export default App;
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = () => {
+    return (
+        <Navbar bg="primary" expand="lg" variant='dark'>
+            <Container>
+                <Navbar.Brand href="/">Khammerson</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className='m-auto'>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                        </Form>
+                    </Nav>
+                    <Nav
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action2">WorkSpace</Nav.Link>
+                        <NavDropdown title="Khamit Amantaev" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action4">
+                                My Profile
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Logout
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header
