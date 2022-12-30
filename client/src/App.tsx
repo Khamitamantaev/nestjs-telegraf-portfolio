0f86758fa1888882fa2e7e585cac9a813cb37040
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2022-12-31 01:12:29 +0600

    add simple routes and products page
    add react-router-dom to dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,19 @@ import './bootstrap.min.css'
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import LandingPage from './screens/LandingPage/LandingPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import MyProducts from './screens/MyProducts/MyProducts';
 
-const App = () => 
-<div className="App">
-  <Header/>
-  <main>
-    <LandingPage/>
-  </main>
-  <Footer/>
-</div>
+const App = () =>
+  <BrowserRouter>
+    <Header />
+    <main>
+      <Routes>
+        <Route path='/' element={<LandingPage />}  />
+        <Route path='/products' element={<MyProducts />} />
+      </Routes>
+    </main>
+    <Footer />
+  </BrowserRouter>
 
 export default App;
